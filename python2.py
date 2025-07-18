cf3dc073b87b2bfb4a843276010a146823745350
Azeem117 <adwardcollin365@gmail.com>
2025-07-18 18:48:03 +0530

    b

--- python2.py
+++ python2.py
@@ -1,6 +1,12 @@
-name = "Md azeem"
+name = "Md Azeem"
 age = 23
 price = 78.008
 print(name)
 print(age +2)
 print(price+78)
+print(type(name))
+print(type(age))
+aged  = False
+a = None
+print(type(aged))
+print(type(a))
