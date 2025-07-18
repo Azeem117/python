6b68bf095fa6037c06bfd62bc251b66a97b3e920
Azeem117 <adwardcollin365@gmail.com>
2025-07-18 19:03:26 +0530

    a

--- python3.py
+++ python3.py
@@ -3,3 +3,9 @@ b=3
 print(a+b)
 sum = a+b
 print(sum)
+
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+
---
+++ python4.py
@@ -0,0 +1,31 @@
+a= 24
+b = 12
+# Arithmatic operators
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a%b)
+
+print(a**b) # a to the powwer b
+
+#relational operator
+
+print(a==b)
+
+print(a!=b)
+
+print(a>=b)
+
+print(a<=b)
+
+# logical operator
+
+print(not a)
+
+print(not b)
