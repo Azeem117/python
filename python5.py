34efd3281400b7ccc765b4875d4cf2fcb492dec3
Azeem117 <adwardcollin365@gmail.com>
2025-07-19 18:45:08 +0530

    a

---
+++ python5.py
@@ -0,0 +1,17 @@
+# logical opertors
+
+# print(not False)
+# print(not True)
+# a = 20
+# b = 10
+# print(not (a>b))
+# print(not (a<b))
+
+v = True
+u = True
+
+print(u and v)
+
+w = True
+x = False
+print(w or x)
